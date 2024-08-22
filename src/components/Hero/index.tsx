98fe2182211766b546985a8ae76e36b95063696f
Reinaldo <reinaldo18junior@gmail.com>
2024-08-22 15:48:12 -0300

    convertendo estilização com styled components

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,16 @@
-import styles from './Hero.module.css'
+import { Container } from '../../styles'
+import { FormDiv, FormularioHero, HeroTitle } from './styles'
 
 const Hero = () => (
-  <form className={styles.form}>
-    <div className="container">
-      <h2 className={styles.heroTitle}>
-        As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
-    </div>
-  </form>
+  <FormularioHero>
+    <Container>
+      <FormDiv>
+        <HeroTitle>
+          As melhores vagas para tecnologia, design e artes visuais.
+        </HeroTitle>
+      </FormDiv>
+    </Container>
+  </FormularioHero>
 )
 
 export default Hero
